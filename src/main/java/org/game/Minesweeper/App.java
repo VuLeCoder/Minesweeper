e7d5fceb50a3cd0f9f7345ee54593674def35229
VuzLe <tle63917@gmail.com>
2025-03-07 15:31:56 +0700

    ko có gì

--- src/main/java/org/game/Minesweeper/App.java
+++ src/main/java/org/game/Minesweeper/App.java
@@ -2,9 +2,8 @@ package org.game.Minesweeper;
 
 public class App {
 	public static void main(String[] args) {
-
 		System.setProperty("sun.java2d.uiScale", "1.0");
 		
-		new Game(0);
+		new Game(1);
 	}
 }
